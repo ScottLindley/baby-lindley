8f38c205d5adfd7addda8a374877836d9130a158
Scott Lindley <scottdlindley@gmail.com>
2019-09-06 09:00:58 -0400

    clean up comments

--- src/components/Confetti.svelte
+++ src/components/Confetti.svelte
@@ -9,13 +9,13 @@
 
     class Confetti {
       constructor() {
-        const colours = ["#fde132", "#bb85d2", "#7aab80"];
+        const colors = ["#fde132", "#bb85d2", "#7aab80"];
         const size = Math.random(10) * (w / 60);
         this.x = Math.round(Math.random(10) * w);
         this.y = Math.round(Math.random(10) * h) - h / 2;
         this.rotation = Math.random(10) * 360;
         this.size = size < 15 ? 15 : size;
-        this.color = colours[Math.round(Math.random(colours.length) * 10 - 1)];
+        this.color = colors[Math.round(Math.random(colors.length) * 10 - 1)];
         this.speed = this.size / 7;
         this.opacity = Math.random(10);
         this.shiftDirection = Math.random(10) > 0.5 ? 1 : -1;
